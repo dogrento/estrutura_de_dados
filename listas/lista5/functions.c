0e70cfa16a74e438926f9aab79ce4d24b445051e
dogrento <dogrento93@gmail.com>
2022-09-19 07:49:44 -0300

    finalizando ex3

--- listas/lista5/functions.c
+++ listas/lista5/functions.c
@@ -13,5 +13,23 @@ int in(List* l, int k){
         return 1;
     }
     else if(l!=NULL)
-        in(l->next, k);
+        return in(l->next, k);
+}
+
+//ex3
+List* merge(List* a, List* b){
+    if(a==NULL)
+        return b;
+    if(b==NULL)
+        return a;
+
+    if(a->data < b->data){
+        a->next = merge(a->next, b);
+        return a;
+    }
+
+    else{
+        b->next = merge(a, b->next);
+        return b;
+    }
 }
